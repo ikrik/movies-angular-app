2b0e523579f7153bbe959756eaea407ccbef87ef
Krik Tsakma <gregtsakma@gmail.com>
2026-01-31 15:05:30 +0200

    feat(config): add environment-based configuration

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://api.themoviedb.org/3' ,
+  apiToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YWUyNmM3MTk3MTk2NzA3NWU2NTRiY2RlMTVkYmZiOCIsIm5iZiI6MTc2OTg1NjExMi4yNzEwMDAxLCJzdWIiOiI2OTdkZGM3MDViNWFiNGVlNTczZTRlMWYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.UAuwwYU1t80wuUCGCIcichRiktblokJMdB8oLbKVUd4'
+};
