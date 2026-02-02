e622f1802da790a3b2d2f96bae497287dfcfa608
Krik Tsakma <gregtsakma@gmail.com>
2026-02-02 16:11:49 +0200

    feat(user-menu): add reset stores action

--- src/widgets/movies-header/user-menu/user-menu.ts
+++ src/widgets/movies-header/user-menu/user-menu.ts
@@ -1,9 +1,11 @@
+import { FavoritesStore } from "@entities/movie/model/favorites-store";
 import { Router } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { Component, ChangeDetectionStrategy, inject } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatMenuModule } from "@angular/material/menu";
+import { MoviesStore } from "@entities/movie/model/movies-store";
 
 @Component({
   selector: "movies-user-menu",
@@ -14,8 +16,15 @@ import { MatMenuModule } from "@angular/material/menu";
 })
 export class MoviesUserMenu {
   private readonly router = inject(Router);
+  private readonly favoritesStore = inject(FavoritesStore);
+  private readonly moviesStore = inject(MoviesStore);
 
   favouritesList() {
     void this.router.navigate(["/favorites"]);
+  }
+
+  resetState() {
+    this.favoritesStore.clear();
+    this.moviesStore.clear();
   }
 }
